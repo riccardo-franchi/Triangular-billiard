4b467dff6d0c4dc0ac1d78930e8e8ef7b3a4ce17
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-05-23 12:57:58 +0200

    Change constructor

--- src/particle.cpp
+++ src/particle.cpp
@@ -2,6 +2,7 @@
 #include "../include/billiard.hpp"
 #include "../include/point.hpp"
 
-Particle::Particle(double x, double y, double vx, double vy) : m_p{x, y}, m_vx{vx}, m_vy{vy} {}
+Particle::Particle(double y0, double v0x, double v0y) //
+	: m_p{0., y0}, m_vx{v0x}, m_vy{v0y} {};
 
 Particle collision(Particle particle, Billiard billiard) {}
