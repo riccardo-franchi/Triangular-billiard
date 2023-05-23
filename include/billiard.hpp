924c741650ea282cf5d09b82cb3707e5247aca62
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-05-23 12:39:30 +0200

    Add exception in billiard class

--- include/billiard.hpp
+++ include/billiard.hpp
@@ -1,6 +1,19 @@
+#include <stdexcept>
+
 class Billiard
 {
-	int r1;		// y-value of the left end of the billiard
-	int r2;		// y-value of the right end of the billiard
-	int lenght; // x-value of the right end of the billiard
+public:
+	Billiard(double r1, double r2, double l) //
+		: m_r1{r1}, m_r2{r2}, m_l{l}		 //
+	{
+		if (m_r1 < 0 || m_r2 < 0 || m_l < 0)
+		{
+			throw std::domain_error("Negative argument in constructor");
+		}
+	}
+
+private:
+	double m_r1{};
+	double m_r2{};
+	double m_l{};
 };
