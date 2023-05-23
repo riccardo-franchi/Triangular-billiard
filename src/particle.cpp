1bf6b1f0fc05be5b8cf0e39047a6d0b494b93ad2
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-05-23 12:51:25 +0200

    fix includes

--- src/particle.cpp
+++ src/particle.cpp
@@ -1,6 +1,6 @@
-#include "particle.hpp"
-#include "billiard.hpp"
-#include "point.hpp"
+#include "../include/particle.hpp"
+#include "../include/billiard.hpp"
+#include "../include/point.hpp"
 
 Particle::Particle(double x, double y, double vx, double vy) : m_p{x, y}, m_vx{vx}, m_vy{vy} {}
 
