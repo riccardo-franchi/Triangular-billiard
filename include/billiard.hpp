da461b5d751cd2fcd13f83d1c493406ed2b54481
Giorgio Germano <giorgio.germano.ravina@gmail.com>
2023-05-23 11:58:39 +0200

    Created Billiard class

---
+++ include/billiard.hpp
@@ -0,0 +1,6 @@
+class Billiard
+{
+	int r1;		// y-value of the left end of the billiard
+	int r2;		// y-value of the right end of the billiard
+	int lenght; // x-value of the right end of the billiard
+};
