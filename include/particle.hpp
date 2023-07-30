a17802bcfb4c0cb7af6b1bc510f2cdb7d2cb817f
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-07-30 22:37:30 +0200

    Revert previous change and edit Particle class

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,14 +1,24 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
 
+#include <numbers>
+#include <stdexcept>
+
 class Particle
 {
 public:
-	explicit Particle(double y0);
+	explicit Particle(double y0, double theta) : m_x0{0.0}, m_y0{y0}, m_theta{theta}
+	{
+		if (m_theta < -std::numbers::pi || m_theta > std::numbers::pi)
+		{
+			throw std::invalid_argument{"theta must be in [-pi, pi]"};
+		}
+	}
 
 private:
 	double m_x0;
 	double m_y0;
+	double m_theta;
 };
 
 #endif // PARTICLE_HPP
--- src/particle.cpp
+++
@@ -1,3 +0,0 @@
-#include "../include/particle.hpp"
-
-Particle::Particle(double y0) : m_x0{0}, m_y0{y0} {}
