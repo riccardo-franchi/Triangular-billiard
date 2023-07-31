65450830beb5404d77d5a1ea24f94894ab66e0f5
Giorgio Germano <giorgio.germano.ravina@gmail.com>
2023-07-31 08:09:12 +0200

    Added the collide function

--- src/particle.cpp
+++ src/particle.cpp
@@ -9,8 +9,22 @@ Particle::Particle(double y0, double theta) : m_p{0., y0}, m_theta{theta} //
 {
 	if (theta > M_PI / 2 || theta < -M_PI / 2)
 	{
-		throw std::domain_error("Invalid angol");
+		throw std::domain_error("Invalid angle");
 	}
 };
 
-Particle collision(Particle particle, Billiard billiard) {}
+bool collide(Particle particle, Billiard billiard)
+{
+
+	// check if the particle excapes the billiard
+	double theta_min{billiard.getR2() * particle.getPoint().y / (billiard.getL() - particle.getPoint().x)};
+	double theta_max{billiard.getR1() * particle.getPoint().y / (billiard.getL() - particle.getPoint().x)};
+	if (particle.getTheta() < theta_max || particle.getTheta() > theta_min)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
