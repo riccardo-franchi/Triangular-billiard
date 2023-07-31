4b2b46b275b67b7e505bd6afd5d3cb9daa017b7d
Marta Barbieri <marta.barbi@gmail.com>
2023-07-31 09:11:32 +0200

    Rimosso classe Point

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,23 +1,20 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
 
-#include "point.hpp"
-
 class Particle
 {
 public:
 	Particle(double y0, double theta);
-	Point getPoint() const { return m_p; }
+	double getX() const { return m_x; }
+	double getY() const { return m_y; }
 	double getTheta() const { return m_theta; }
-	void setPoint(double x, double y)
-	{
-		m_p.x = x;
-		m_p.y = y;
-	}
+	void setX(double x) { m_x = x; }
+	void setY(double y) { m_y = y; }
 	void setTheta(double theta) { m_theta = theta; }
 
 private:
-	Point m_p;
+	double m_x;
+	double m_y;
 	double m_theta;
 };
 
--- src/particle.cpp
+++ src/particle.cpp
@@ -1,11 +1,10 @@
 #include "../include/particle.hpp"
 #include "../include/billiard.hpp"
-#include "../include/point.hpp"
 
 #include <cmath>
 #include <stdexcept>
 
-Particle::Particle(double y0, double theta) : m_p{0., y0}, m_theta{theta} //
+Particle::Particle(double y0, double theta) : m_x{0}, m_y{y0}, m_theta{theta} //
 {
 	if (theta > M_PI / 2 || theta < -M_PI / 2)
 	{
@@ -16,8 +15,8 @@ Particle::Particle(double y0, double theta) : m_p{0., y0}, m_theta{theta} //
 bool collide(Particle particle, Billiard billiard)
 {
 	// check if the particle stays in the billiard
-	double theta_min{billiard.getR2() * particle.getPoint().y / (billiard.getL() - particle.getPoint().x)};
-	double theta_max{billiard.getR1() * particle.getPoint().y / (billiard.getL() - particle.getPoint().x)};
+	double theta_min{billiard.getR2() * particle.getY() / (billiard.getL() - particle.getX())};
+	double theta_max{billiard.getR1() * particle.getY() / (billiard.getL() - particle.getX())};
 	if (particle.getTheta() < theta_max || particle.getTheta() > theta_min)
 	{
 		return false;
