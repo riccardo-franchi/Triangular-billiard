0371d87751cfe93970afd156afd87dcde31ae63a
Giorgio Germano <giorgio.germano.ravina@gmail.com>
2023-08-04 23:14:27 +0200

    Added tests for 0 and 1 collision(s)

--- tests/billiard.test.cpp
+++ tests/billiard.test.cpp
@@ -17,13 +17,27 @@ TEST_CASE("Testing the Billiard constructor")
 	CHECK_THROWS(Billiard{1., 1., -1.});
 }
 
-TEST_CASE("Three collisions, theta>0")
+TEST_CASE("Testing the runSimulation() function")
 {
 	Billiard billiard{5., 3., 13.};
-	billiard.setParticle({2.63, 0.5613});
 
-	SUBCASE("Testing the runSimulation() function")
+	SUBCASE("No collisions")
 	{
+		billiard.setParticle({-2.47, 0.32083});
+		billiard.runSimulation();
+		CHECK(approx_eq(billiard.getParticle(), {13., 1.85, 0.32083}));
+	}
+
+	SUBCASE("One collision, theta > 0")
+	{
+		billiard.setParticle({1.12534, 0.41964});
+		billiard.runSimulation();
+		CHECK(approx_eq(billiard.getParticle(), {13., -1.78878, -0.72494}));
+	}
+
+	SUBCASE("Three collisions, theta > 0")
+	{
+		billiard.setParticle({2.63, 0.5613});
 		billiard.runSimulation();
 		CHECK(approx_eq(billiard.getParticle(), {13., -1.18987, -1.4772}));
 	}
