a4e960eb54acaf210131b0de759c1f606e46bcfd
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-08-04 18:26:36 +0200

    Add missing dots

--- tests/billiard.test.cpp
+++ tests/billiard.test.cpp
@@ -19,12 +19,12 @@ TEST_CASE("Testing the Billiard constructor")
 
 TEST_CASE("Three collisions, theta>0")
 {
-	Billiard billiard{5, 3, 13};
+	Billiard billiard{5., 3., 13.};
 	billiard.setParticle({2.6, 0.561298});
 
 	SUBCASE("Testing the runSimulation() function")
 	{
 		billiard.runSimulation();
-		CHECK(approx_eq(billiard.getParticle(), {13, -1.19, -1.477194}));
+		CHECK(approx_eq(billiard.getParticle(), {13., -1.19, -1.477194}));
 	}
 }
