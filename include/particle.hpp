dea17f7ee551bbe81e52a44771eb162ef86bebef
Marta Barbieri <marta.barbi@gmail.com>
2023-08-04 12:36:26 +0200

    conditions on theta

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,11 +1,24 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
+#include <cmath>
 
 struct Particle
 {
 public:
-	Particle(double x0, double y0, double theta) : x{x0}, y{y0}, theta{theta} {}
-	Particle(double y0, double theta) : Particle(0, y0, theta) {}
+	Particle(double x0, double y0, double theta) : x{x0}, y{y0}, theta{theta}
+	{
+		if (std::abs(theta) > M_PI / 2)
+		{
+			throw std::domain_error{"Invalid angle"};
+		}
+	}
+	Particle(double y0, double theta) : Particle(0, y0, theta)
+	{
+		if (std::abs(theta) > M_PI / 2)
+		{
+			throw std::domain_error{"Invalid angle"};
+		}
+	}
 
 	double x{};
 	double y{};
