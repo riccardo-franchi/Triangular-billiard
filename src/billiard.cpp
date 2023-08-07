ab6ac0d40e3a0c84a5a8894c53b109fdc555fbbb
Giorgio Germano <giorgio.germano.ravina@gmail.com>
2023-08-07 23:55:31 +0900

    removed m_particles

--- src/billiard.cpp
+++ src/billiard.cpp
@@ -37,8 +37,8 @@ Particle Billiard::calcTrajectory(const Particle& p, const double alpha)
 
 	while (std::abs(yl) > m_r2)
 	{
-		const double theta{(yl > m_r2) ? 2. * alpha - m_particle.theta //
-									   : -2. * alpha - m_particle.theta};
+		const double theta{(yl > m_r2) ? 2. * alpha - particle.theta //
+									   : -2. * alpha - particle.theta};
 
 		if (std::abs(theta) > M_PI_2)
 		{
@@ -50,7 +50,7 @@ Particle Billiard::calcTrajectory(const Particle& p, const double alpha)
 		const double xi{(yl > m_r2) ? (coeff * particle.x + m_r1 - particle.y) / (coeff + ((m_r1 - m_r2) / m_l))
 									: (coeff * particle.x - m_r1 - particle.y) / (coeff + ((m_r2 - m_r1) / m_l))};
 
-		const double yi{coeff * (xi - m_particle.x) + m_particle.y};
+		const double yi{coeff * (xi - particle.x) + particle.y};
 
 		particle = {xi, yi, theta};
 
