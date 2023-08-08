b674f7f5758391bcc5b53716bd608574d91cfd80
Marta Barbieri <marta.barbi@gmail.com>
2023-08-08 15:37:24 +0200

    General corrections

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,34 @@
-#include <algorithm>
-#include <iomanip>
+#include <fstream>
 #include <iostream>
+#include <random>
 
-#include "billiard.hpp"
+#include "../include/billiard.hpp"
+#include "../include/statistics.hpp"
 
 int main()
 {
-	Billiard billiard{};
+	double r1{};
+	double r2{};
+	double l{};
+
+	std::cin >> r1;
+	std::cin >> r2;
+	std::cin >> l;
+
+	Billiard billiard{r1, r2, l};
 	std::random_device r;
 	std::default_random_engine engine(r());
 	std::normal_distribution<double> dist;
 
 	int N{};
-	std::cin >> "Specify the number of entries:" >> N;
+	std::cin >> N;
 
 	for (int n{0}; n != N; ++n)
 	{
-		billiard.push_back(dist(engine));
+		Particle particle{dist(engine), dist(engine)};
+		billiard.push_back(particle);
 	}
 
-	const auto stat{billiard.statistics()};
+	const auto stat{statistics(billiard.getParticles())};
 	std::cout << "- mean: " << stat.mean << '\n' << "- sigma: " << stat.sigma << '\n';
 }
