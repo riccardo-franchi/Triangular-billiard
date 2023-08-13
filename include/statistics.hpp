2823c2b2277dcf1d319ad4b564d78345ebf071f8
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-08-13 16:03:34 +0200

    fix #17

--- include/statistics.hpp
+++ include/statistics.hpp
@@ -22,18 +22,18 @@ struct Statistics
 
 struct Sums
 {
-	double yf;
-	double yf2;
-	double th;
-	double th2;
+	double yf{};
+	double yf2{};
+	double th{};
+	double th2{};
 };
 
 struct Gaps
 {
-	double y3;
-	double th3;
-	double y4;
-	double th4;
+	double y3{};
+	double th3{};
+	double y4{};
+	double th4{};
 };
 
 Statistics statistics(const std::vector<Particle>& particles, const double l)
@@ -54,7 +54,7 @@ Statistics statistics(const std::vector<Particle>& particles, const double l)
 		throw std::runtime_error{"Not enough entries to run a statistics"};
 	}
 
-	sums = std::accumulate(escParts.begin(), escParts.end(), Sums{0., 0., 0., 0.},
+	sums = std::accumulate(escParts.begin(), escParts.end(), Sums{},
 						   [](Sums s, Particle p)
 						   {
 							   s.yf += p.y;
@@ -68,29 +68,17 @@ Statistics statistics(const std::vector<Particle>& particles, const double l)
 	double const sigma_y{std::sqrt((sums.yf2 - N * mean_y * mean_y) / (N - 1))};	// why not mean sigma?
 	double const mean_th{sums.th / N};
 	double const sigma_th{std::sqrt((sums.th2 - N * mean_th * mean_th) / (N - 1))}; // idem
-	// double const mean_err = sigma / std::sqrt(N);
 
-	Gaps gaps{};
-	for (auto i{escParts.begin()}; i != escParts.end(); i++)
-	{
-		Particle p{*i};
-		gaps.y3 += (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y);
-		gaps.y4 += (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y);
-		gaps.th3 += (p.theta - mean_th) * (p.theta - mean_th) * (p.theta - mean_th);
-		gaps.th4 += (p.theta - mean_th) * (p.theta - mean_th) * (p.theta - mean_th) * (p.theta - mean_th);
-	}
-
-	/* cannot use mean_y and mean_th
-		gaps = std::accumulate(p.begin(), p.end(), Gaps{0., 0., 0., 0.}, [](Gaps g, Particle p),
-							   [=](mean_y, mean_th)
-							   {
-								   g.y2 += (p.y - mean_y) * (p.y - mean_y);
-								   g.y2 += (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y);
-								   g.th2 += (p.theta - mean_th) * (p.theta - mean_th);
-								   g.th3 += (p.theta - mean_th) * (p.theta - mean_th) * (p.theta - mean_th);
-								   return g;
-							   });
-							   */
+	const Gaps gaps{std::accumulate(escParts.begin(), escParts.end(), Gaps{},
+									[mean_y, mean_th](Gaps g, Particle p)
+									{
+										g.y3 += (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y);
+										g.y4 += (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y) * (p.y - mean_y);
+										g.th3 += (p.theta - mean_th) * (p.theta - mean_th) * (p.theta - mean_th);
+										g.th4 += (p.theta - mean_th) * (p.theta - mean_th) * (p.theta - mean_th) *
+												 (p.theta - mean_th);
+										return g;
+									})};
 
 	double const skewness_y{gaps.y3 / (N * sigma_y * sigma_y * sigma_y)};
 	double const skewness_th{gaps.th3 / (N * sigma_th * sigma_th * sigma_th)};
