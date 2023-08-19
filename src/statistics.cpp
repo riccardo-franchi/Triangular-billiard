ff0022a84ec58cdd951e66af3da6afc63f9ea187
Riccardo Franchi <richi.franchi2004@gmail.com>
2023-08-19 10:28:58 +0200

    Update statistics, not a class anymore :(

--- src/statistics.cpp
+++ src/statistics.cpp
@@ -16,40 +16,27 @@ struct Moments
 	double x4{};
 };
 
-Statistics::Results Statistics::operator()(const std::vector<Particle>& particles)
+Results statistics(const std::vector<Particle>& particles)
 {
 	if (particles.size() < 2)
 	{
 		throw std::runtime_error{"Not enough particles to compute statistics"};
 	}
 
-	// Create vector of y and theta values of escaped particles
 	std::vector<double> y{};
 	std::vector<double> theta{};
 	y.reserve(particles.size());
 	theta.reserve(particles.size());
 	for (const auto& p : particles)
 	{
-		if (p.x == m_l)
-		{
-			y.push_back(p.y);
-			theta.push_back(p.theta);
-		}
-	}
-	y.shrink_to_fit();
-	theta.shrink_to_fit();
-
-	m_N = static_cast<int>(y.size());
-
-	if (m_N < 2)
-	{
-		throw std::runtime_error{"Cannot compute statistics: " + std::to_string(m_N) + " particle(s) escaped"};
+		y.push_back(p.y);
+		theta.push_back(p.theta);
 	}
 
 	return {computeStats(y), computeStats(theta)};
 }
 
-std::string Statistics::statsToString(const Results& stats)
+std::string statsToString(const Results& stats)
 {
 	const int w{20};
 	std::ostringstream oss{};
@@ -66,10 +53,12 @@ std::string Statistics::statsToString(const Results& stats)
 	return oss.str();
 }
 
-Statistics::Stats Statistics::computeStats(const std::vector<double>& data)
+Stats computeStats(const std::vector<double>& data)
 {
+	const int N{static_cast<int>(data.size())};
+
 	const double sum{std::accumulate(data.begin(), data.end(), 0., [](double s, double x) { return s += x; })};
-	const double mean{sum / m_N};
+	const double mean{sum / N};
 
 	const Moments moments{std::accumulate(data.begin(), data.end(), Moments{},
 										  [mean](Moments m, double x)
@@ -81,9 +70,9 @@ Statistics::Stats Statistics::computeStats(const std::vector<double>& data)
 											  return m;
 										  })};
 
-	const double sigma{std::sqrt(moments.x2 / (m_N - 1))};
-	const double skewness{moments.x3 / (m_N * std::pow(sigma, 3))};
-	const double kurtosis{moments.x4 / (m_N * std::pow(sigma, 4))};
+	const double sigma{std::sqrt(moments.x2 / (N - 1))};
+	const double skewness{moments.x3 / (N * std::pow(sigma, 3))};
+	const double kurtosis{moments.x4 / (N * std::pow(sigma, 4))};
 
 	return {mean, sigma, skewness, kurtosis};
 }
